902573e5f51f2f2b7782e7cb6bf4688b5070d122
Leandro74377 <leandrobaque4@gmail.com>
2025-07-30 01:33:20 -0500

    Update app.js

--- Challenge/app.js
+++ Challenge/app.js
@@ -51,7 +51,4 @@ function sortearAmigo() {
   // Mostrar el resultado en el elemento con id "resultadoSorteo"
   const resultado = document.getElementById("resultado");
   resultado.innerHTML = `Amigo sorteado: <strong>${amigoSorteado}</strong>`;
-  
-  const lista = document.getElementById("listaAmigos");
-  lista.style.display = "none";
 }
